e4e5a6ff21554616ebd312fe7c0c8985d159b1b5
Itay Tamary <33928704+ItayTamary@users.noreply.github.com>
2019-04-30 22:24:50 -0400

    Ability to resize between ticks

--- cell.js
+++ cell.js
@@ -1,39 +1,33 @@
+const MIN_DIM = 7;
 
 
-const CELL_DIMENSION = 30; //20 X 20 PIXEL SQUARE
+var CELL_DIMENSION = 40; //20 X 20 PIXEL SQUARE
 
 var cellGrid = [];
 var COLORS = ["red", "green", "grey", "yellow", "grey"]; // [DEAD, ALIVE, FIXED-DEAD, FIXED-ALIVE, GRIDLINES]
 
 
-
 function cellUniverse() {
-	var tableGrid = document.getElementById('cell-grid');
-
-	var gridHeight = tableGrid.offsetHeight;
-	var gridWidth = tableGrid.offsetWidth;
-	
-	var numRows = gridHeight / CELL_DIMENSION;
-	var numCols = gridWidth / CELL_DIMENSION;
-	
 	this.leftBound = 0;
 	this.topBound = 0;
 
-	this.rightBound = Math.floor(numCols); //TODO: Calculate rightBound
-	this.bottomBound = Math.floor(numRows); //TODO: Calculate bottomBound
-	
-	this.generateCells = function() {
-		for(var i = 0; i <= this.bottomBound; i++) {
-			cellGrid.push( [] );
-			for(var j = 0; j <= this.rightBound; j++) {
-				cellGrid[i].push(new Cell(i, j));
-				if(Math.random()*10 < 11) cellGrid[i][j].setIsAlive(false); 
+	this.rightBound = 0; //TODO: Calculate rightBound
+	this.bottomBound = 0; //TODO: Calculate bottomBound
+
+	this.generateCells = function () {
+		for (var i = 0; i <= this.bottomBound; i++) {
+			if (cellGrid[i] == undefined) cellGrid.push([]);
+			for (var j = 0; j <= this.rightBound; j++) {
+				if (cellGrid[i][j] == undefined) {
+					cellGrid[i].push(new Cell(i, j));
+					cellGrid[i][j].setIsAlive(false);
+				}
 				cellGrid[i][j].updateAppearance();
 			}
 		}
 	}
 
-	this.tick = function() {
+	this.tick = function () {
 		for (var i = 0; i <= this.bottomBound; i++) {
 			for (var j = 0; j <= this.rightBound; j++) {
 				cellGrid[i][j].calculateIsAliveNextRound();
@@ -46,31 +40,61 @@ function cellUniverse() {
 		}
 	}
 
-}
+	this.generateTable = function () {
+		var tableGrid = document.getElementById('cell-grid'); //GENERATE TABLE SHOULD DECIDE ROWS/COLS
+		tableGrid.innerHTML = "";
 
-function generateTable() {
-	var tableGrid = document.getElementById('cell-grid');
+		var numRows = this.bottomBound;
+		var numCols = this.rightBound;
 
-	var gridHeight = tableGrid.offsetHeight;
-	var gridWidth = tableGrid.offsetWidth;
-	
-	var numRows = gridHeight / CELL_DIMENSION;
-	var numCols = gridWidth / CELL_DIMENSION;
-
-	for(var i = 0; i < numRows; i++) {
-		tableGrid.insertRow();
-		var gridRow = tableGrid.rows[i];
-		for(var j = 0; j < numCols; j++) {
-			gridRow.insertCell();
+		for (var i = 0; i <= numRows; i++) {
+			tableGrid.insertRow();
+			var gridRow = tableGrid.rows[i];
+			for (var j = 0; j <= numCols; j++) {
+				gridRow.insertCell();
+			}
 		}
 	}
 
+	this.resize = function (newDim) {
+		CELL_DIMENSION = newDim;
+		this.updateBounds();
+		this.generateTable();
+		this.generateCells()
+	}
+
+	this.updateBounds = function () {
+		var tableGrid = document.getElementById('cell-grid');
+		var gridHeight = tableGrid.offsetHeight;
+		var gridWidth = tableGrid.offsetWidth;
+
+		var numRows = gridHeight / CELL_DIMENSION;
+		var numCols = gridWidth / CELL_DIMENSION;
+
+		this.leftBound = 0;
+		this.topBound = 0;
+
+		this.rightBound = Math.floor(numCols) - 1; //TODO: Calculate rightBound
+		this.bottomBound = Math.floor(numRows) - 1; //TODO: Calculate bottomBound
+	}
 }
 
-function updateTable(y, x, status) {
+function updateCellAppearance(y, x, status) {
 	var tableGrid = document.getElementById('cell-grid');
 	tableGrid.rows[y].cells[x].style.backgroundColor = COLORS[status];
+	
+
 }
+
+
+
+
+
+
+
+
+
+
 
 
 function Cell(y, x) {
@@ -83,56 +107,56 @@ function Cell(y, x) {
 
 
 
-/*****Getters*****/
-	this.getIsAlive = function() {
+	/*****Getters*****/
+	this.getIsAlive = function () {
 		return this.isAlive;
 	}
-	
-	this.getIsFixed = function() {
+
+	this.getIsFixed = function () {
 		return this.isFixed;
 	}
 
-	this.getIsAliveNextRound = function() {
+	this.getIsAliveNextRound = function () {
 		return this.getIsAliveNextRound;
 	}
 
-	this.getYCoordinate = function() {
+	this.getYCoordinate = function () {
 		return this.yCoordinate;
 	}
-	this.getXCoordinate = function() {
+	this.getXCoordinate = function () {
 		return this.xCoordinate
 	}
 
 
-/*****Setters*****/
-	this.setIsAlive = function(toggle) {
+	/*****Setters*****/
+	this.setIsAlive = function (toggle) {
 		this.isAlive = toggle;
 	}
-	
-	this.setIsFixed = function(toggle) {
+
+	this.setIsFixed = function (toggle) {
 		this.isFixed = toggle;
 	}
-	
-	this.setIsAliveNextRound = function(toggle) {
+
+	this.setIsAliveNextRound = function (toggle) {
 		this.isAliveNextRound = toggle;
 	}
-	
-	this.setYCoordinate = function(yCord) {
+
+	this.setYCoordinate = function (yCord) {
 		this.yCoordinate = yCord;
 	}
 
-	this.setXCoordinate = function(xCord) {
+	this.setXCoordinate = function (xCord) {
 		this.xCoordinate = xCord;
 	}
 
-	
-	
-/*****Methods*****/
 
-	this.updateAppearance = function() {
-			updateTable(this.getYCoordinate(), this.getXCoordinate(), Number(this.getIsAlive()));
+
+	/*****Methods*****/
+
+	this.updateAppearance = function () {
+		updateCellAppearance(this.getYCoordinate(), this.getXCoordinate(), Number(this.getIsAlive()));
 	}
-	
+
 	this.calculateIsAliveNextRound = function () {
 		if (this.getIsFixed() === true) {
 			this.setIsAliveNextRound(this.isAlive);
@@ -143,7 +167,7 @@ function Cell(y, x) {
 		else if (adjAlive === 3) this.setIsAliveNextRound(true);
 		else this.setIsAliveNextRound(this.isAlive);
 	}
-	
+
 	this.calculateAdjAliveCount = function () {
 		var countAlive = Number(this.checkTop()) + Number(this.checkBottom()) + Number(this.checkLeft()) + Number(this.checkRight()) + Number(this.checkTopLeft()) + Number(this.checkTopRight()) + Number(this.checkBottomLeft()) + Number(this.checkBottomRight());
 		return countAlive;
@@ -151,65 +175,63 @@ function Cell(y, x) {
 
 	/*Check Status of Neighbors*/
 
-	this.checkTop = function() {
-		
+	this.checkTop = function () {
 		var topAdjY = this.getYCoordinate() - 1;
-		if(topAdjY < grid.topBound) topAdjY = grid.bottomBound;
-
+		if (topAdjY < world.topBound) topAdjY = world.bottomBound;
 		return cellGrid[topAdjY][this.xCoordinate].getIsAlive();
 	}
 
-	this.checkBottom = function() {
+	this.checkBottom = function () {
 		var bottomAdjY = this.getYCoordinate() + 1;
-		if(bottomAdjY > grid.bottomBound) bottomAdjY = grid.topBound;
+		if (bottomAdjY > world.bottomBound) bottomAdjY = world.topBound;
 		return cellGrid[bottomAdjY][this.getXCoordinate()].getIsAlive();
 	}
 
-	this.checkLeft = function() {
+	this.checkLeft = function () {
 		var leftAdjX = this.getXCoordinate() - 1;
-		if(leftAdjX < grid.leftBound) leftAdjX = grid.rightBound;
+		if (leftAdjX < world.leftBound) leftAdjX = world.rightBound;
 		return cellGrid[this.getYCoordinate()][leftAdjX].getIsAlive();
 	}
 
-	this.checkRight = function() {
+	this.checkRight = function () {
 		var rightAdjX = this.getXCoordinate() + 1;
-		if(rightAdjX > grid.rightBound) rightAdjX = grid.leftBound;
+		if (rightAdjX > world.rightBound) rightAdjX = world.leftBound;
 		return cellGrid[this.getYCoordinate()][rightAdjX].getIsAlive();
 	}
 
-	this.checkTopLeft = function() {
+	this.checkTopLeft = function () {
 		var topAdjY = this.getYCoordinate() - 1;
-		if(topAdjY < grid.topBound) topAdjY = grid.bottomBound;
+		if (topAdjY < world.topBound) topAdjY = world.bottomBound;
 		var leftAdjX = this.getXCoordinate() - 1;
-		if(leftAdjX < grid.leftBound) leftAdjX = grid.rightBound;
+		if (leftAdjX < world.leftBound) leftAdjX = world.rightBound;
 		return cellGrid[topAdjY][leftAdjX].getIsAlive();
 	}
 
-	this.checkTopRight = function() {
+	this.checkTopRight = function () {
 		var topAdjY = this.getYCoordinate() - 1;
-		if(topAdjY < grid.topBound) topAdjY = grid.bottomBound;
+		if (topAdjY < world.topBound) topAdjY = world.bottomBound;
 		var rightAdjX = this.getXCoordinate() + 1;
-		if(rightAdjX > grid.rightBound) rightAdjX = grid.leftBound;
+		if (rightAdjX > world.rightBound) rightAdjX = world.leftBound;
 		return cellGrid[topAdjY][rightAdjX].getIsAlive();
 	}
 
-	this.checkBottomLeft = function() {
+	this.checkBottomLeft = function () {
 		var bottomAdjY = this.getYCoordinate() + 1;
-		if(bottomAdjY > grid.bottomBound) bottomAdjY = grid.topBound;
+		if (bottomAdjY > world.bottomBound) bottomAdjY = world.topBound;
 		var leftAdjX = this.getXCoordinate() - 1;
-		if(leftAdjX < grid.leftBound) leftAdjX = grid.rightBound;
+		if (leftAdjX < world.leftBound) leftAdjX = world.rightBound;
 		return cellGrid[bottomAdjY][leftAdjX].getIsAlive();
 	}
 
-	this.checkBottomRight = function() {
+	this.checkBottomRight = function () {
 		var bottomAdjY = this.getYCoordinate() + 1;
-		if(bottomAdjY > grid.bottomBound) bottomAdjY = grid.topBound;
+		if (bottomAdjY > world.bottomBound) bottomAdjY = world.topBound;
 		var rightAdjX = this.getXCoordinate() + 1;
-		if(rightAdjX > grid.rightBound) rightAdjX = grid.leftBound;
+		if (rightAdjX > world.rightBound) rightAdjX = world.leftBound;
 		return cellGrid[bottomAdjY][rightAdjX].getIsAlive();
-	} 
+	}
 
-	this.tick = function() {
+	this.tick = function () {
 		this.setIsAlive(this.isAliveNextRound);
 		this.updateAppearance();
 	}
@@ -222,7 +244,7 @@ function demo() {
 	cellGrid[6][4].isAlive = true;
 	cellGrid[6][5].isAlive = true;
 	cellGrid[6][6].isAlive = true;
-	
+
 	cellGrid[4][5].updateAppearance();
 	cellGrid[5][6].updateAppearance();
 	cellGrid[6][4].updateAppearance();
@@ -232,12 +254,21 @@ function demo() {
 }
 
 function tick() {
-	grid.tick();
+	world.tick();
 }
 
-generateTable();
-var grid = new cellUniverse();
-grid.generateCells();
+function changeSize() {
+	var newSize = document.getElementById("size").value;
+	world.resize(newSize);
+}
+
+var world = new cellUniverse();
+world.resize(CELL_DIMENSION);
 document.getElementById("ticker").addEventListener("click", tick);
+document.getElementById("size").defaultValue = CELL_DIMENSION;
+document.getElementById("confirm-size").addEventListener("click", changeSize);
+
+
+
 demo();
 
