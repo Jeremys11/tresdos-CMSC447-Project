188cce2ef258c19808f9abcfa0bf5597c1d4ce0b
JENO <uberjoel25@yahoo.com>
2019-05-06 23:11:36 -0400

    Next

--- TextHandler.js
+++ TextHandler.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
             "debug": true,
             "fallbackLng" : 'en',
             backend: {
-                loadPath: 'https://jeremys11.github.io/tresdos-CMSC447-Project/locales/{{lng}}/{{ns}}.json'
+                loadPath: '{{lng}}/{{ns}}.json'
             }
         },
         function(err,t){
