4912db38b3dcdc5facf395e2323215dc6151e98d
JENO <uberjoel25@yahoo.com>
2019-05-14 12:14:07 -0400

    Added Comments
    Added a couple lines of comments

--- TextHandler.js
+++ TextHandler.js
@@ -2,7 +2,9 @@ import jqueryI18next from "jquery-i18next";
 import i18next from "i18next";
 import XHR from "i18next-xhr-backend";
 
+//on page load
 $(document).ready(function(){
+    //initialization code taken from https://github.com/i18next/jquery-i18next/blob/master/README.md
     jqueryI18next.init(i18next, $, {
         tName: 't',
         i18nName: 'i18n',
@@ -13,6 +15,7 @@ $(document).ready(function(){
         useOptionsAttr: false,
         parseDefaultValueFromContent: true});
 
+        //initialize our document language to english. Get translations from 'loadPath'
         i18next
         .use(XHR)
         .init({
@@ -23,16 +26,21 @@ $(document).ready(function(){
                 loadPath: 'locales/{{lng}}/{{ns}}.json'
             }
         },
+        //throw error if initialization fails
         function(err,t){
             if(err) return console.error(err)
             $(document).localize();
         });
 
+        //langSelector is our dropdown box
         $('.langSelector').change(function() {
+            //get the value from our dropdown and store it into lang
             var lang = $(this).val();
+            //change language to whatever the value is
             i18next.init({
                 lng : lang
             }, function (t){
+                //translate document using language
                 $(document).localize();
             })
         })
